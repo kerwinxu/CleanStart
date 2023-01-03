a5ee2b10f6719600a16ce42160503217015dd855
恒晓 徐 <kerwin.cn@gmail.com>
2023-01-03 21:04:57 +0800

    Add project files.

---
+++ 干净启动/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace 干净启动
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_service_disabled = new System.Windows.Forms.Button();
+            this.btn_service_recover = new System.Windows.Forms.Button();
+            this.txt_log = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_service_disabled
+            // 
+            this.btn_service_disabled.Location = new System.Drawing.Point(57, 12);
+            this.btn_service_disabled.Name = "btn_service_disabled";
+            this.btn_service_disabled.Size = new System.Drawing.Size(254, 23);
+            this.btn_service_disabled.TabIndex = 0;
+            this.btn_service_disabled.Text = "All Service Disabled(if can stop)";
+            this.btn_service_disabled.UseVisualStyleBackColor = true;
+            this.btn_service_disabled.Click += new System.EventHandler(this.btn_service_disabled_Click);
+            // 
+            // btn_service_recover
+            // 
+            this.btn_service_recover.Location = new System.Drawing.Point(57, 57);
+            this.btn_service_recover.Name = "btn_service_recover";
+            this.btn_service_recover.Size = new System.Drawing.Size(254, 23);
+            this.btn_service_recover.TabIndex = 1;
+            this.btn_service_recover.Text = "All Service Recover";
+            this.btn_service_recover.UseVisualStyleBackColor = true;
+            // 
+            // txt_log
+            // 
+            this.txt_log.Location = new System.Drawing.Point(12, 106);
+            this.txt_log.Multiline = true;
+            this.txt_log.Name = "txt_log";
+            this.txt_log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_log.Size = new System.Drawing.Size(340, 309);
+            this.txt_log.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 439);
+            this.Controls.Add(this.txt_log);
+            this.Controls.Add(this.btn_service_recover);
+            this.Controls.Add(this.btn_service_disabled);
+            this.Name = "Form1";
+            this.Text = "clean start";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_service_disabled;
+        private System.Windows.Forms.Button btn_service_recover;
+        private System.Windows.Forms.TextBox txt_log;
+    }
+}
+
---
+++ 干净启动/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.ServiceProcess;
+
+namespace 干净启动
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+
+        }
+
+        private void btn_service_disabled_Click(object sender, EventArgs e)
+        {
+            var lst_services = ServiceController.GetServices().ToList();
+            foreach (var service in lst_services)
+            {
+                // 这里要保存原先的状态
+                var strs = lst_services.Select(x => x.DisplayName).ToArray();
+                var str = string.Join(Environment.NewLine, strs);
+                txt_log.Text = str;
+            }
+        }
+    }
+}
